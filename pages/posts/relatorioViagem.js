9b87a9bc48c64aa6859540e86f05a6d1eb1fc98a
luizpibo <75041533+luizpibo@users.noreply.github.com>
2021-02-01 15:12:21 -0300

    Update relatorioViagem.js

--- pages/posts/relatorioViagem.js
+++ pages/posts/relatorioViagem.js
@@ -1,15 +1,23 @@
 //imports
-import Head from 'next/head'
 import Link from 'next/link'
 import Layout, {siteTitle} from '../components/layout'
+import Classname from 'classnames'
+
 export default function RelatorioViagem(){
     return(
         <>
             <header>
                 <Link href="\">Home</Link>
-                <input type="text" id="fname" name="fname" value="John"></input>
             </header>
                 
         </>   
+    )
+}
+
+function relatorio(){
+    return(
+        <div Classname="">
+
+        </div>
     )
 }
